8622ba77b93dd6b5d2e3f0aa6c2eec3c90f5210f
gustighn <gustigani903@gmail.com>
2025-09-17 14:06:52 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import "./App.css";
-import "./output.css";
 
 export default function App() {
   const [items, setItems] = useState([]);
